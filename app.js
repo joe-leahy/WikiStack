92b91d7074ea6aa683f447e1f3605e16b7f56e73
joe-leahy <josleahy@icloud.com>
2022-11-21 12:02:31 -0500

    Creating the HTML Forms

--- app.js
+++ app.js
@@ -1,17 +1,36 @@
 const express = require('express')
 const app = express();
+const bodyParser = require('body-parser');
+const urlencodedParser = bodyParser.urlencoded({extended: false})
+const { db, Page, User } = require('./models');
+const wikiRouter = require('./routes/wiki');
+const userRouter = require('./routes/users')
 
 const morgan = require('morgan')
 
 app.use(express.static("public"));
 app.use(morgan("dev"));
+app.use(urlencodedParser);
+
+app.use('/wiki', wikiRouter);
+app.use('/users', userRouter);
 
 
-app.get('/', (req,res) => {
-  res.send('Hello World!')
+db.authenticate()
+  .then(() => {
+    console.log('connected to the database');
 })
 
-const PORT = 1337;
-app.listen(PORT, () => {
-  console.log(`App is listening in Port: ${PORT}`)
+app.get('/', (req,res, next) => {
+  res.redirect('/wiki')
 })
+
+const PORT = 3000;
+const init = async () => {
+  await db.sync({force: true});
+  app.listen(PORT, () => {
+    console.log(`Server is listening in Port: ${PORT}`)
+  })
+}
+
+init();
---
+++ views/addPage.js
@@ -0,0 +1,35 @@
+const html = require("html-template-tag");
+const layout = require("./layout");
+
+module.exports = () => layout(html`
+  <h3>Add a Page</h3>
+  <hr>
+  <form method="POST" action="/wiki/">
+
+    <div><label for="title" class="col-sm-2 control-label">Name</label>
+    <div class="col-sm-10">
+      <input id="title" name="name" type="text" class="form-control"/></div>
+
+    <div><label for="title" class="col-sm-2 control-label">Email</label>
+    <div class="col-sm-10">
+      <input id="title" name="email" type="text" class="form-control"/></div>
+
+    <div class="form-group">
+      <label for="title" class="col-sm-2 control-label">Page Title</label>
+      <div class="col-sm-10">
+        <input id="title" name="title" type="text" class="form-control"/>
+      </div>
+    </div>
+
+    <div><label for="title" class="col-sm-2 control-label">Page Content</label>
+    <div class="col-sm-10">
+      <input id="title" name="content" type="text" class="form-control"/></div>
+
+    <div>PLACEHOLDER FOR PAGE STATUS INPUT FIELD</div>
+
+    <div class="col-sm-offset-2 col-sm-10">
+      <button type="submit" class="btn btn-primary">submit</button>
+    </div>
+
+  </form>
+`);
